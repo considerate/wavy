b882c8f95571f5a0a693f650628a70f446df6d5b
Robert Massaioli <robertmassaioli@gmail.com>
2013-05-02 23:19:20 +1000

    Made it possible to parse the standard headers of the RIFF file format.
    Maybe, in the future, this project will become RIFFy or sometihng like that. But for the
    here and now my focus is getting the audio data out of WAV files. Once this is all said
    and done then I can look into doing more.

--- Sound/Wav.hs
+++ Sound/Wav.hs
@@ -2,7 +2,12 @@ module Sound.Wav where
 
 import System.IO
 import Data.Binary
+import Data.Binary.Get
+import Data.Bits (shiftL, shiftR, (.|.))
+import Data.List (unfoldr)
+import Data.Word
 import Data.Char
+import Control.Monad (guard, replicateM)
 import Sound.Wav.Data
 
 parseData :: FilePath -> IO Header
@@ -31,15 +36,50 @@ instance Binary SectionOne where
       put chunkSize
       put "WAVE"
 
-   get = do
-      riffHeader <- sequence (take 4 . repeat $ getWord8)
-      -- This is an error because Binary decodes in big endian format :(
-      -- See how hard it would be to get Binary to decode in both formats
-      chunkSize <- get
-      wavHeader <- sequence (take 4 . repeat $ getWord8)
-      return $ SOne chunkSize
-      
+   get = getSectionOne
 
+instance Binary SectionTwo where
+   put _ = error ""
+
+   get = getSectionTwo
+
+-- TODO write a MonadPlus instance for Data.Binary
+
+getNWords :: Int -> Get [Word8]
+getNWords n = replicateM n getWord8
+
+getNChars :: Int -> Get [Char]
+getNChars = fmap (fmap byteToChar) . getNWords 
+
+-- TODO Clean this up with monad transformers
+getSectionOne :: Get SectionOne
+getSectionOne = do
+   riffHeader <- getNChars 4
+   if (riffHeader /= "RIFF") 
+      then fail "Expected RIFF file structure"
+      else do
+         chunkSize <- getWord32le
+         wavHeader <- getNChars 4
+         if (wavHeader /= "WAVE")
+            then fail "Expected WAVE file to be present in header."
+            else return $ SOne chunkSize
+
+
+getSectionTwo :: Get SectionTwo
+getSectionTwo = do
+   fmtHeader <- getNChars 4
+   if fmtHeader /= "fmt "
+      then fail "Expected the beginning of the format section."
+      else do
+         chunkSize <- getWord32le
+         audio <- getWord16le
+         channelCount <- getWord16le
+         sampleRateData <- getWord32le
+         byteRateData <- getWord32le
+         blockAlign <- getWord16le
+         bitsPerSample <- getWord16le
+         return $ SectionTwo chunkSize audio channelCount sampleRateData byteRateData blockAlign bitsPerSample
+   
 {-
 parseSectionOne :: Binary SectionOne
 parseSectionOne = do
@@ -48,6 +88,3 @@ parseSectionOne = do
    waveComment <- sequence (take 4 . repeat . getWord8)
    return $ SOne chunkSize
    -}
-
-
-
--- Sound/Wav/Data.hs
+++ Sound/Wav/Data.hs
@@ -3,9 +3,26 @@ module Sound.Wav.Data where
 import Data.Word
 
 type ChunkSize = Word32
+type AudioFormat = Word16
+type SampleRate = Word32
+type ByteRate = Word32
+type BlockAlignment = Word16
+type BitsPerSample = Word16
 
 data Header = Header Bool
             deriving(Show)
 
 data SectionOne = SOne ChunkSize
                 deriving(Show)
+
+data SectionTwo = SectionTwo
+   -- The size of the rest of the chunk after this point...is this data useful in the final result?
+   { sectionSize :: ChunkSize 
+   , audioFormat :: AudioFormat -- Values other than one indicate some form of compression
+   , numChannels :: Word16 
+   , sampleRate :: SampleRate
+   , byteRate :: ByteRate
+   , blockAlignment :: BlockAlignment
+   , bitsPerSample :: BitsPerSample
+   }
+   deriving(Show)

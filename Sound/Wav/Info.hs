5a47cebe6c068ad0533530d9f56062d3f4cf0ae7
Robert Massaioli <robertmassaioli@gmail.com>
2013-05-05 09:35:47 +1000

    Allowed the parsing of separated strings.

--- Sound/Wav/Info.hs
+++ Sound/Wav/Info.hs
@@ -3,6 +3,7 @@ module Sound.Wav.Info where
 
 import Data.Binary (Get(..), Binary(..))
 import Data.Binary.Get
+import Data.List.Split (splitOn)
 
 import Data.Word
 
@@ -43,19 +44,16 @@ parseSection initial = do
       "IARL" -> (\x -> return $ initial { archiveLocation = ValidRFV x}) =<< parseInfoString
       "IART" -> (\x -> return $ initial { artist = ValidRFV x}) =<< parseInfoString
       "ICMS" -> (\x -> return $ initial { commissionedBy = ValidRFV x}) =<< parseInfoString
-      -- TODO use splitOn from package 'split'
-      -- "ICMT" -> (\x -> return $ initial { comments = ValidRFV x}) =<< parseInfoString
-      -- "ICOP" -> (\x -> return $ initial { copyrights = ValidRFV x}) =<< parseInfoString
+      "ICMT" -> (\x -> return $ initial { comments = ValidRFV x}) =<< parseInfoString
+      "ICOP" -> (\x -> return $ initial { copyrights = ValidRFV x}) =<< parseInfoStrings
       "ICRD" -> (\x -> return $ initial { creationDate = ValidRFV x}) =<< parseInfoString
       "ICRP" -> (\x -> return $ initial { croppedDetails = ValidRFV x}) =<< parseInfoString
       "IDIM" -> (\x -> return $ initial { originalDimensions = ValidRFV x}) =<< parseInfoString
       -- TODO parse this as an integer, needs to unroll data
       -- "IDPI" -> (\x -> return $ initial { dotsPerInch = ValidRFV x}) =<< parseInfoString
-      -- TODO another list that needs list parsing
-      -- "IENG" -> (\x -> return $ initial { engineers = ValidRFV x}) =<< parseInfoString
+      "IENG" -> (\x -> return $ initial { engineers = ValidRFV x}) =<< parseInfoStrings
       "IGNR" -> (\x -> return $ initial { genre = ValidRFV x}) =<< parseInfoString
-      -- TODO another list that needs list parsing
-      -- "IKEY" -> (\x -> return $ initial { keywords = ValidRFV x}) =<< parseInfoString
+      "IKEY" -> (\x -> return $ initial { keywords = ValidRFV x}) =<< parseInfoStrings
       "ILGT" -> (\x -> return $ initial { lightness = ValidRFV x}) =<< parseInfoString
       "IMED" -> (\x -> return $ initial { originalMedium = ValidRFV x}) =<< parseInfoString
       "INAM" -> (\x -> return $ initial { name = ValidRFV x}) =<< parseInfoString
@@ -78,6 +76,8 @@ parseInfoString = do
    infoString <- getNChars (fromIntegral . makeEven $ chunkSize)
    return $ dropTrailingNull infoString
 
+parseInfoStrings :: Get [String]
+parseInfoStrings = fmap (splitOn "; ") parseInfoString
 
 {-
 -- TODO This is the Maybe Monad Transformer right here

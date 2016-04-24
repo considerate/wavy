c1312710bdd4d716252eb9aac1dba219d1555ec6
Robert Massaioli <rmassaioli@atlassian.com>
2016-04-25 08:57:57 +1000

    Updated the identity program to make it more complete.

--- src/Identity.hs
+++ src/Identity.hs
@@ -1,17 +1,21 @@
 module Main where
 
-import Sound.Wav
+import           Sound.Wav
 
-import System.Environment
+import           System.Environment
+import           System.FilePath    (splitExtension)
 
 main = do
    args <- getArgs
    case args of
       [] -> putStrLn "You need to provide a file for this to work."
-      (x:_) -> decodeWaveFileOrFail x >>= writeWaveFile
+      (x:_) -> decodeWaveFileOrFail x >>= writeWaveFile x >> putStrLn ("Generated identity file: " ++ generateName x)
 
-writeWaveFile :: Either a WaveFile -> IO ()
-writeWaveFile (Left _) = print "Could not even parse the file let alone output it."
--- TODO Instead of always using output.wav let the user specify what they want the output
--- file to be.
-writeWaveFile (Right file) = encodeWaveFile "output.wav" file
+generateName :: String -> String
+generateName original = f ++ "-identity" ++ e
+   where
+      (f, e) = splitExtension original
+
+writeWaveFile :: String -> Either a WaveFile -> IO ()
+writeWaveFile filename (Left _) = print $ "Could not even parse '" ++ filename ++ "' let alone output it."
+writeWaveFile filename (Right file) = encodeWaveFile (generateName filename) file

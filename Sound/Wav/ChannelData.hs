2cbe8c2e4d3ae8de9418cfe2f0abc248cffa1a1d
Viktor Kronvall <viktor.kronvall@gmail.com>
2018-06-01 15:27:04 +0900

    Bump versions

--- Sound/Wav/ChannelData.hs
+++ Sound/Wav/ChannelData.hs
@@ -2,7 +2,7 @@
 -- audio data from WAVE files or put audio data into wave files and your data may be in integral or
 -- floating formats. These methods make it trivial for you to put that data inside WaveFiles. This
 -- module is designed to make the manipulation of the channel data inside a wave file easier.
-module Sound.Wav.ChannelData 
+module Sound.Wav.ChannelData
    ( getWaveData
    , extractIntegralWaveData
    , extractFloatingWaveData
@@ -18,8 +18,8 @@ import Sound.Wav.Scale
 
 import Control.Monad (replicateM)
 import Control.Applicative ((<$>))
-import Data.Binary.Get
-import Data.Binary.Put
+import Data.Binary.Get hiding(getInt8, getInt16le, getInt32le, getInt64le)
+import Data.Binary.Put hiding(putInt8, putInt16le, putInt32le, putInt64le)
 import Data.Bits
 import Data.List (transpose)
 import Data.List.Split (chunksOf)
@@ -27,76 +27,75 @@ import Data.Word
 import Data.Int
 import qualified Data.Vector as V
 
-emptyRawWaveData = FloatingWaveData []
-emptyIntegralWaveData = IntegralWaveData []
+-- emptyRawWaveData = FloatingWaveData []
+-- emptyIntegralWaveData = IntegralWaveData []
 
 -- | Given a WaveFile you will either get back an integral lossless representation of the audio data
 -- or you will get back a parser error.
-extractIntegralWaveData 
-   :: WaveFile                                  -- ^ The wave file that contains the data that you wish to extract.
-   -> Either WaveParseError IntegralWaveData    -- ^ The final result, either an error or the data that you were looking for.
+extractIntegralWaveData
+  :: WaveFile                                  -- ^ The wave file that contains the data that you wish to extract.
+  -> Either WaveParseError IntegralWaveData    -- ^ The final result, either an error or the data that you were looking for.
 extractIntegralWaveData waveFile = case runGetOrFail getter rawData of
-   Left (_, offset, error) -> Left $ error ++ " (" ++ show offset ++ ")"
-   Right (_, _, parsedData) -> Right parsedData
-   where
-      rawData = waveData waveFile
-      getter = getWaveDataIntegral (waveFormat waveFile)
+  Left  (_, offset, error     ) -> Left $ error ++ " (" ++ show offset ++ ")"
+  Right (_, _     , parsedData) -> Right parsedData
+  where
+    rawData = waveData waveFile
+    getter  = getWaveDataIntegral (waveFormat waveFile)
 
 -- | Extracts the data in the WaveFile in the floating format. This method allows you to view the
 -- audio data in this file in the range [-1, 1] so that you can process a normalised view of the
 -- data no matter how it was internally encoded. Be aware that the conversion to floating point and
 -- back will be a lossy operation.
-extractFloatingWaveData 
-   :: WaveFile                                  -- ^ The WaveFile that contains the data that you wish to extract.
-   -> Either WaveParseError FloatingWaveData
+extractFloatingWaveData
+  :: WaveFile                                  -- ^ The WaveFile that contains the data that you wish to extract.
+  -> Either WaveParseError FloatingWaveData
 extractFloatingWaveData waveFile = case runGetOrFail getter rawData of
-   Left (_, offset, error) -> Left $ error ++ " (" ++ show offset ++ ")"
-   Right (_, _, parsedData) -> Right parsedData
-   where
-      rawData = waveData waveFile
-      getter = getWaveDataFloating $ waveFormat waveFile
+  Left  (_, offset, error     ) -> Left $ error ++ " (" ++ show offset ++ ")"
+  Right (_, _     , parsedData) -> Right parsedData
+  where
+    rawData = waveData waveFile
+    getter  = getWaveDataFloating $ waveFormat waveFile
 
 -- TODO If the WaveFile has a fact chunk then we should update it at this point
 -- | Given a WaveFile replace it's data contents with the integral data that you provide.
-encodeIntegralWaveData 
-   :: WaveFile          -- ^ The WaveFile that you wish to fill with data.
-   -> IntegralWaveData  -- ^ The integral data to be placed inside the WaveFile.
-   -> WaveFile          -- ^ A WaveFile containing the data that you provided (formatted correctly).
+encodeIntegralWaveData
+  :: WaveFile          -- ^ The WaveFile that you wish to fill with data.
+  -> IntegralWaveData  -- ^ The integral data to be placed inside the WaveFile.
+  -> WaveFile          -- ^ A WaveFile containing the data that you provided (formatted correctly).
 encodeIntegralWaveData file rawData = file { waveData = runPut putter }
-   where
-      putter = putIntegralWaveData (waveFormat file) rawData
+  where putter = putIntegralWaveData (waveFormat file) rawData
 
 -- | Given a WaveFile replace it's data contents with the floating data that you provide.
-encodeFloatingWaveData 
-   :: WaveFile          -- ^ The WaveFile that will be used to contain the provided data.
-   -> FloatingWaveData  -- ^ The floating data to be placed inside the WaveFile. Will be converted to integrals internally and will therefore be a lossy process.
-   -> WaveFile          -- ^ A WaveFile containing the data that you provided (formatted correctly).
+encodeFloatingWaveData
+  :: WaveFile          -- ^ The WaveFile that will be used to contain the provided data.
+  -> FloatingWaveData  -- ^ The floating data to be placed inside the WaveFile. Will be converted to integrals internally and will therefore be a lossy process.
+  -> WaveFile          -- ^ A WaveFile containing the data that you provided (formatted correctly).
 encodeFloatingWaveData file rawData = file { waveData = runPut putter }
-   where
-      putter = putFloatingWaveData (waveFormat file) rawData
+  where putter = putFloatingWaveData (waveFormat file) rawData
 
 -- | A putter for integral wave data given a format so that you can output correctly formatted data
 -- to any stream.
-putIntegralWaveData 
-   :: WaveFormat        -- ^ The format that the wave data should be encoded into.
-   -> IntegralWaveData  -- ^ The integral data that should be pushed out to the stream.
-   -> Put               -- ^ A Put context that will be used to write the data out.
-putIntegralWaveData format (IntegralWaveData rawData) = mapM_ putter $ interleaveData rawData
-   where
-      putter :: Int64 -> Put
-      putter = wordPutter (bytesPerChannelSample format)
+putIntegralWaveData
+  :: WaveFormat        -- ^ The format that the wave data should be encoded into.
+  -> IntegralWaveData  -- ^ The integral data that should be pushed out to the stream.
+  -> Put               -- ^ A Put context that will be used to write the data out.
+putIntegralWaveData format (IntegralWaveData rawData) = mapM_ putter
+  $ interleaveData rawData
+  where
+    putter :: Int64 -> Put
+    putter = wordPutter (bytesPerChannelSample format)
 
 -- | A putter for floating wave data given a format so that you can output correctly formatted data
 -- to any stream.
-putFloatingWaveData 
-   :: WaveFormat        -- ^ The format that the WAVE data should be encoded into.
-   -> FloatingWaveData  -- ^ The floating data that should be pushed out to the stream.
-   -> Put               -- ^ A Put context that will be used to write the data out.
-putFloatingWaveData format (FloatingWaveData rawData) = 
-   mapM_ (putter . floatToInt) $ interleaveData rawData
-   where
-      putter :: Int64 -> Put
-      putter = wordPutter (bytesPerChannelSample format) 
+putFloatingWaveData
+  :: WaveFormat        -- ^ The format that the WAVE data should be encoded into.
+  -> FloatingWaveData  -- ^ The floating data that should be pushed out to the stream.
+  -> Put               -- ^ A Put context that will be used to write the data out.
+putFloatingWaveData format (FloatingWaveData rawData) = mapM_ (putter . floatToInt)
+  $ interleaveData rawData
+  where
+    putter :: Int64 -> Put
+    putter = wordPutter (bytesPerChannelSample format)
 
 bytesPerChannelSample :: WaveFormat -> Word16
 bytesPerChannelSample format = waveBitsPerSample format `divRoundUp` 8
@@ -128,18 +127,18 @@ zeroStable64 = zeroStable (0 :: Int64)
 -- This process is invertible without loss of data.
 getWaveDataIntegral :: WaveFormat -> Get IntegralWaveData
 getWaveDataIntegral format = fmap convertData (getWaveData format)
-   where
-      convertData :: [[Int64]] -> IntegralWaveData
-      convertData = IntegralWaveData . fmap V.fromList
+  where
+    convertData :: [[Int64]] -> IntegralWaveData
+    convertData = IntegralWaveData . fmap V.fromList
 
 -- | Sometimes when you are dealing with audio data you want to get the numbers as floating values
 -- in the range [-1, 1]. This method will allow you to parse the data out of a WaveFile straight
 -- into a floating point format. Please be aware that this process is lossy.
 getWaveDataFloating :: WaveFormat -> Get FloatingWaveData
 getWaveDataFloating format = fmap convertData (getWaveData format)
-   where
-      convertData :: [[Int64]] -> FloatingWaveData
-      convertData = FloatingWaveData . fmap (V.fromList . fmap intToFloat)
+  where
+    convertData :: [[Int64]] -> FloatingWaveData
+    convertData = FloatingWaveData . fmap (V.fromList . fmap intToFloat)
 
 intToFloat :: Int64 -> Double
 intToFloat x = fromIntegral x / fromIntegral (maxBound :: Int64)
@@ -148,24 +147,24 @@ floatToInt :: Double -> Int64
 floatToInt x = round $ x * fromIntegral (maxBound :: Int64)
 
 -- | Generate a getter that will parse a WAVE "data " chunk from a raw stream given a wave format.
-getWaveData 
-   :: WaveFormat     -- ^ The format that the data is in.
-   -> Get [[Int64]]  -- ^ The audio data, normalised into a stardard container.
+getWaveData
+  :: WaveFormat     -- ^ The format that the data is in.
+  -> Get [[Int64]]  -- ^ The audio data, normalised into a stardard container.
 getWaveData format = do
-   dataLength <- remaining
-   let readableWords = fromIntegral $ dataLength `div` bytesPerChannelSample
-   (transpose . chunksOf channels) <$> getNWords readableWords
-   where
-      getNWords :: Int -> Get [Int64]
-      getNWords words = replicateM words $ wordGetter bytesPerChannelSample
+  dataLength <- remaining
+  let readableWords = fromIntegral $ dataLength `div` bytesPerChannelSample
+  (transpose . chunksOf channels) <$> getNWords readableWords
+  where
+    getNWords :: Int -> Get [Int64]
+    getNWords words = replicateM words $ wordGetter bytesPerChannelSample
 
-      channels :: Int
-      channels = fromIntegral $ waveNumChannels format
+    channels :: Int
+    channels = fromIntegral $ waveNumChannels format
 
-      bytesPerChannelSample :: Int64
-      bytesPerChannelSample = fromIntegral $ waveBitsPerSample format `divRoundUp` 8
+    bytesPerChannelSample :: Int64
+    bytesPerChannelSample = fromIntegral $ waveBitsPerSample format `divRoundUp` 8
 
-divRoundUp ::  Integral a => a -> a -> a
+divRoundUp :: Integral a => a -> a -> a
 divRoundUp num den = case num `divMod` den of
-   (x, 0) -> x
-   (x, _) -> x + 1
+  (x, 0) -> x
+  (x, _) -> x + 1
